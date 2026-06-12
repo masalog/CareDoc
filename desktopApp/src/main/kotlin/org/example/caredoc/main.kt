25d76d8b5fadbc4a57018571b49da3f5ee133c6a
masalog <masalog@example.com>
2026-06-12 20:52:12 +0900

    feat : main.kt アプリの起動を早めるためにフォントスキャンを無効化

--- desktopApp/src/main/kotlin/org/example/caredoc/main.kt
+++ desktopApp/src/main/kotlin/org/example/caredoc/main.kt
@@ -4,6 +4,11 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
+
+    // PDFBox のフォントスキャンを無効化
+    System.setProperty("pdfbox.fontcache", "false")
+    System.setProperty("pdfbox.skipFontLoading", "true")
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "caredoc",
