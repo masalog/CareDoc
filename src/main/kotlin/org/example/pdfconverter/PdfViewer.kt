229f18647037a075c36486c497c89f0cadbb6577
masalog <masalog@example.com>
2026-06-18 19:38:40 +0900

    refactor : PdfViewer.kt UIコンポーネントを縦に並べて配置するためのVBoxをインポート

--- src/main/kotlin/org/example/pdfconverter/PdfViewer.kt
+++ src/main/kotlin/org/example/pdfconverter/PdfViewer.kt
@@ -7,6 +7,7 @@ import javafx.scene.control.*
 import javafx.scene.image.ImageView
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
 import javafx.stage.FileChooser
 import javafx.stage.Stage
 import org.apache.pdfbox.pdmodel.PDDocument
