fad082455bc7886032c12abc8ea0a1971e51eaa3
masalog <masalog@example.com>
2026-06-20 19:27:11 +0900

    refactor : PdfViewer member変数を条件式の中で用いることで、名前を選択ボタンが押された時のみ、ＰＤＦ上にデータが表示されるように変更

--- src/main/kotlin/org/example/pdfconverter/PdfEditor.kt
+++ src/main/kotlin/org/example/pdfconverter/PdfEditor.kt
@@ -9,11 +9,11 @@ import java.io.InputStream
 class PdfEditor {
 
     fun editPdf(
-        member: Member,
+        member: Member?,
         common: CommonData,
         applyYear: Int?,
         applyMonth: Int?,
-        applyDay: Int?
+        applyDay: Int?,
         changeRequestReason: String?
     ): File {
 
@@ -50,77 +50,74 @@ class PdfEditor {
                     }
 
                     // 個別データ
-                    drawText("Insurance ID Number", member.insuranceIdNumber)
-                    drawText("name", member.name)
-                    drawText("furigana", member.furigana)
+                    if (member != null) {
 
-                    member.birthYear?.let { drawText("birthYear", it.toString()) }
-                    member.birthMonth?.let { drawText("birthMonth", it.toString()) }
-                    member.birthDay?.let { drawText("birthDay", it.toString()) }
+                        drawText("Insurance ID Number", member.insuranceIdNumber)
+                        drawText("name", member.name)
+                        drawText("furigana", member.furigana)
 
-                    drawText("address", member.address)
-                    drawText("phone", member.phone)
+                        member.birthYear?.let { drawText("birthYear", it.toString()) }
+                        member.birthMonth?.let { drawText("birthMonth", it.toString()) }
+                        member.birthDay?.let { drawText("birthDay", it.toString()) }
 
-                    when (member.gender) {
-                        "男" -> drawCircle("genderMale")
-                        "女" -> drawCircle("genderFemale")
-                    }
+                        drawText("address", member.address)
+                        drawText("phone", member.phone)
 
-                    // 要介護区分
-                    val careKey = when (member.careLevel) {
-                        "要介護1" -> "Long-term Care Level 1"
-                        "要介護2" -> "Long-term Care Level 2"
-                        "要介護3" -> "Long-term Care Level 3"
-                        "要介護4" -> "Long-term Care Level 4"
-                        "要介護5" -> "Long-term Care Level 5"
-                        "要支援1" -> "Support Level 1"
-                        "要支援2" -> "Support Level 2"
-                        else -> null
-                    }
-                    careKey?.let { drawCircle(it) }
+                        when (member.gender) {
+                            "男" -> drawCircle("genderMale")
+                            "女" -> drawCircle("genderFemale")
+                        }
 
-                    // 申請日
-                    applyYear?.let { drawText("applyYear", it.toString()) }
-                    applyMonth?.let { drawText("applyMonth", it.toString()) }
-                    applyDay?.let { drawText("applyDay", it.toString()) }
+                        val careKey = when (member.careLevel) {
+                            "要介護1" -> "Long-term Care Level 1"
+                            "要介護2" -> "Long-term Care Level 2"
+                            "要介護3" -> "Long-term Care Level 3"
+                            "要介護4" -> "Long-term Care Level 4"
+                            "要介護5" -> "Long-term Care Level 5"
+                            "要支援1" -> "Support Level 1"
+                            "要支援2" -> "Support Level 2"
+                            else -> null
+                        }
+                        careKey?.let { drawCircle(it) }
 
-                    // 有効期間
-                    member.startYear?.let { drawText("startYear", it.toString()) }
-                    member.startMonth?.let { drawText("startMonth", it.toString()) }
-                    member.startDay?.let { drawText("startDay", it.toString()) }
+                        // 有効期間
+                        member.startYear?.let { drawText("startYear", it.toString()) }
+                        member.startMonth?.let { drawText("startMonth", it.toString()) }
+                        member.startDay?.let { drawText("startDay", it.toString()) }
 
-                    member.endYear?.let { drawText("endYear", it.toString()) }
-                    member.endMonth?.let { drawText("endMonth", it.toString()) }
-                    member.endDay?.let { drawText("endDay", it.toString()) }
+                        member.endYear?.let { drawText("endYear", it.toString()) }
+                        member.endMonth?.let { drawText("endMonth", it.toString()) }
+                        member.endDay?.let { drawText("endDay", it.toString()) }
 
-                    // 共通データ
-                    drawText("facilityName", common.facilityName)
-                    drawText("facilityPhone", common.facilityPhone)
-                    drawText("institutionName", common.institutionName)
-                    drawText("institutionAddress", common.institutionAddress)
-                    drawText("agentName", common.agentName)
-                    drawText("agentPostal", common.agentPostal)
-                    drawText("agentAddress", common.agentAddress)
-                    drawText("agentPhone", common.agentPhone)
-                    drawText("doctorName", common.doctorName)
-                    drawText("clinicName", common.clinicName)
-                    drawText("clinicPostal", common.clinicPostal)
-                    drawText("clinicAddress", common.clinicAddress)
-                    drawText("clinicPhone", common.clinicPhone)
-                        // 特定疾病
-                        drawText("specificDisease", member.specificDisease ?: "")
-
-                        drawText("Survey Location Address", common.surveyAddress)
-                        drawText("Survey Location Phone", common.surveyPhone)
                         // 入所日
                         member.institutionYear?.let { drawText("institutionYear", it.toString()) }
                         member.institutionMonth?.let { drawText("institutionMonth", it.toString()) }
                         member.institutionDay?.let { drawText("institutionDay", it.toString()) }
 
+                        // 特定疾病
+                        drawText("specificDisease", member.specificDisease ?: "")
 
-                    drawCircle("isFacility")
-                    drawCircle("agentCategory")
-                }
+                        // 共通データ
+                        drawText("Survey Location Address", common.surveyAddress)
+                        drawText("Survey Location Phone", common.surveyPhone)
+                        drawText("facilityName", common.facilityName)
+                        drawText("facilityPhone", common.facilityPhone)
+                        drawText("institutionName", common.institutionName)
+                        drawText("institutionAddress", common.institutionAddress)
+                        drawText("agentName", common.agentName)
+                        drawText("agentPostal", common.agentPostal)
+                        drawText("agentAddress", common.agentAddress)
+                        drawText("agentPhone", common.agentPhone)
+                        drawText("doctorName", common.doctorName)
+                        drawText("clinicName", common.clinicName)
+                        drawText("clinicPostal", common.clinicPostal)
+                        drawText("clinicAddress", common.clinicAddress)
+                        drawText("clinicPhone", common.clinicPhone)
+
+                        drawCircle("isFacility")
+                        drawCircle("agentCategory")
+                    }
+
 
                     applyYear?.let { drawText("applyYear", it.toString()) }
                     applyMonth?.let { drawText("applyMonth", it.toString()) }
